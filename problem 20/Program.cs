3e07c5eb2812b7008f67f8c7d9ea47d07a49cab5
username <username@cognizant.com>
2022-08-20 23:38:15 +0530

    Add project files.

---
+++ problem 20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Computer";
+            string str2 = "Laptop";
+
+
+            Console.WriteLine(str1 == str2);
+            Console.ReadLine();
+        }
+    }
+}
